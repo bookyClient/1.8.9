3f6ccd21d7cd8ed5f002752847ae81fd96ab30f0
booky10 <github@booky.tk>
2021-01-07 14:25:21 +0100

    added loading plugin back

---
+++ src/main/java/tk/bookyclient/bookyclient/core/ClientFMLSetupHook.java
@@ -0,0 +1,27 @@
+package tk.bookyclient.bookyclient.core;
+// Created by booky10 in bookyClient (21:40 29.12.20)
+
+import net.minecraftforge.fml.relauncher.IFMLCallHook;
+import org.spongepowered.asm.launch.MixinBootstrap;
+import org.spongepowered.asm.mixin.Mixins;
+import tk.bookyclient.bookyclient.utils.Constants;
+
+import java.util.Map;
+
+public class ClientFMLSetupHook implements IFMLCallHook {
+
+    @Override
+    public void injectData(Map<String, Object> data) {
+    }
+
+    @Override
+    public Void call() {
+        Constants.LOGGER.info("Injecting Mixins...");
+
+        MixinBootstrap.init();
+        Mixins.addConfiguration("mixins.bookyclient.json");
+
+        Constants.LOGGER.info("Successfully injected Mixins!");
+        return null;
+    }
+}
