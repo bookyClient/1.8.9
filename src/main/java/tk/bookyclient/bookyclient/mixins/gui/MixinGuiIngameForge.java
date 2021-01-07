71743b79d47272fcf400b08d9ac49c963fa75922
booky10 <github@booky.tk>
2021-01-07 14:20:17 +0100

    display playerlist in singleplayer

---
+++ src/main/java/tk/bookyclient/bookyclient/mixins/gui/MixinGuiIngameForge.java
@@ -0,0 +1,45 @@
+package tk.bookyclient.bookyclient.mixins.gui;
+// Created by booky10 in bookyClient (22:44 04.01.21)
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiIngame;
+import net.minecraft.scoreboard.ScoreObjective;
+import net.minecraftforge.client.GuiIngameForge;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+import static net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType.PLAYER_LIST;
+
+@Mixin(GuiIngameForge.class)
+public abstract class MixinGuiIngameForge extends GuiIngame {
+
+    public MixinGuiIngameForge() {
+        super(Minecraft.getMinecraft());
+    }
+
+    @Shadow(remap = false)
+    protected abstract void post(RenderGameOverlayEvent.ElementType type);
+
+    @Shadow(remap = false)
+    protected abstract boolean pre(RenderGameOverlayEvent.ElementType type);
+
+    /**
+     * @author booky10
+     */
+    @Overwrite(remap = false)
+    protected void renderPlayerList(int width, int height) {
+        ScoreObjective scoreobjective = mc.theWorld.getScoreboard().getObjectiveInDisplaySlot(0);
+
+        if (mc.gameSettings.keyBindPlayerList.isKeyDown()) {
+            overlayPlayerList.updatePlayerList(true);
+            if (pre(PLAYER_LIST)) return;
+
+            overlayPlayerList.renderPlayerlist(width, mc.theWorld.getScoreboard(), scoreobjective);
+
+            post(PLAYER_LIST);
+        } else
+            overlayPlayerList.updatePlayerList(false);
+    }
+}
