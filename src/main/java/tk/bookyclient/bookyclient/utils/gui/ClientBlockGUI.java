b99d75934f7f37c49b57f34c7b3d69bc6d65157d
booky10 <github@booky.tk>
2021-01-07 14:23:27 +0100

    made a simple gray field

---
+++ src/main/java/tk/bookyclient/bookyclient/utils/gui/ClientBlockGUI.java
@@ -0,0 +1,63 @@
+package tk.bookyclient.bookyclient.utils.gui;
+
+import java.io.Serializable;
+
+public class ClientBlockGUI implements Serializable {
+
+    private int left, right, top, bottom;
+
+    public ClientBlockGUI(int left, int right, int top, int bottom) {
+        this.left = left;
+        this.right = right;
+        this.top = top;
+        this.bottom = bottom;
+    }
+
+    public int getWidth() {
+        return right - left;
+    }
+
+    public int getHeight() {
+        return bottom - top;
+    }
+
+    public boolean isMouseOver(int x, int y) {
+        return x >= left && x <= right && y >= top && y <= bottom;
+    }
+
+    public ClientBlockGUI scale(double scale) {
+        return new ClientBlockGUI((int) (left * scale), (int) (right * scale), (int) (top * scale), (int) (bottom * scale));
+    }
+
+    public int getLeft() {
+        return left;
+    }
+
+    public void setLeft(int left) {
+        this.left = left;
+    }
+
+    public int getRight() {
+        return right;
+    }
+
+    public void setRight(int right) {
+        this.right = right;
+    }
+
+    public int getTop() {
+        return top;
+    }
+
+    public void setTop(int top) {
+        this.top = top;
+    }
+
+    public int getBottom() {
+        return bottom;
+    }
+
+    public void setBottom(int bottom) {
+        this.bottom = bottom;
+    }
+}
