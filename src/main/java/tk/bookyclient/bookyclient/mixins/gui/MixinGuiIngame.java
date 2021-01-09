8176741166a5f87b772401412a7e5d014412187c
booky10 <github@booky.tk>
2021-01-09 12:44:25 +0100

    implemented setting clearScoreboardBackground

--- src/main/java/tk/bookyclient/bookyclient/mixins/gui/MixinGuiIngame.java
+++ src/main/java/tk/bookyclient/bookyclient/mixins/gui/MixinGuiIngame.java
@@ -9,10 +9,14 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import tk.bookyclient.bookyclient.settings.ClientSettings;
 
 @Mixin(GuiIngame.class)
 public class MixinGuiIngame extends Gui {
+
+    private static final ClientSettings settings = ClientSettings.getInstance();
 
     @Shadow
     @Final
@@ -21,5 +25,11 @@ public class MixinGuiIngame extends Gui {
     @Inject(method = "showCrosshair", at = @At("HEAD"), cancellable = true)
     public void onShowCrosshair(CallbackInfoReturnable<Boolean> returnable) {
         if (mc.gameSettings.thirdPersonView != 0) returnable.setReturnValue(false);
+    }
+
+    @Redirect(method = "renderScoreboard", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiIngame;drawRect(IIIII)V"))
+    public void onDrawRect(int left, int top, int right, int bottom, int color) {
+        if (settings.clearScoreboardBackground) return;
+        drawRect(left, top, right, bottom, color);
     }
 }
