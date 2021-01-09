3f20b6401439c9f60a4432fcacd2abb0c4040195
booky10 <github@booky.tk>
2021-01-09 14:13:46 +0100

    implemented hideScoreboardRedScores feature

--- src/main/java/tk/bookyclient/bookyclient/mixins/gui/MixinGuiIngame.java
+++ src/main/java/tk/bookyclient/bookyclient/mixins/gui/MixinGuiIngame.java
@@ -2,8 +2,10 @@ package tk.bookyclient.bookyclient.mixins.gui;
 // Created by booky10 in bookyClient (13:52 07.01.21)
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiIngame;
+import net.minecraft.util.EnumChatFormatting;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -30,5 +32,13 @@ public class MixinGuiIngame extends Gui {
     @Redirect(method = "renderScoreboard", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiIngame;drawRect(IIIII)V"), expect = 3)
     public void onDrawRect(int left, int top, int right, int bottom, int color) {
         drawRect(left, top, right, bottom, settings.clearScoreboardBackground ? 0 : color);
+    }
+
+    @Redirect(method = "renderScoreboard", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/FontRenderer;drawString(Ljava/lang/String;III)I"), expect = 3)
+    public int onDrawString(FontRenderer fontRenderer, String text, int x, int y, int color) {
+        if (!text.startsWith(EnumChatFormatting.RED.toString())) return fontRenderer.drawString(text, x, y, color);
+        if (settings.hideScoreboardRedScores) return 0;
+
+        return fontRenderer.drawString(text, x, y, color);
     }
 }
