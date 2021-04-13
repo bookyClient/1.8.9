d2c88665f32d83f683a63cf6880b363316693a26
booky10 <github@booky.tk>
2021-04-13 18:44:32 +0200

    cleaned up the code

--- src/main/java/tk/bookyclient/bookyclient/mixins/forge/MixinFMLCommonHandler.java
+++ src/main/java/tk/bookyclient/bookyclient/mixins/forge/MixinFMLCommonHandler.java
@@ -5,31 +5,16 @@ import com.google.common.collect.ImmutableList;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import tk.bookyclient.bookyclient.utils.Constants;
-
-import java.util.List;
 
 @Mixin(FMLCommonHandler.class)
 public class MixinFMLCommonHandler {
 
-    @Shadow(remap = false)
-    private List<String> brandings;
-
-    @SuppressWarnings("unused")
-    @Shadow(remap = false)
-    private List<String> brandingsNoMC;
-
-    @Inject(method = "computeBranding", at = @At(value = "RETURN", shift = At.Shift.BY, by = -1), remap = false)
-    public void onComputeBranding(CallbackInfo callbackInfo) {
-        ImmutableList.Builder<String> brd = ImmutableList.builder();
-        brd.addAll(brandings);
-        brd.add("bookyClient " + Constants.VERSION + ", MC " + Minecraft.getMinecraft().getVersion());
-
-        brandings = brd.build();
-        brandingsNoMC = brandings.subList(1, brandings.size());
+    @Redirect(remap = false, method = "computeBranding", at = @At(value = "INVOKE_ASSIGN", target = "Lcom/google/common/collect/ImmutableList$Builder;build()Lcom/google/common/collect/ImmutableList;"))
+    public ImmutableList<String> onModsAdd(ImmutableList.Builder<String> builder) {
+        builder.add(String.format("%s %s (MC %s)", Constants.MOD_NAME, Constants.VERSION, Minecraft.getMinecraft().getVersion()));
+        return builder.build();
     }
 }
